15a046c379c21d7bd0bcabb5975d2f5c33f6c921
Allan Jardine <allan@datatables.net>
2026-06-23 15:22:38 +0000

    Initial commit

---
+++ js/cardView.semanticui.js
@@ -0,0 +1,57 @@
+/*! CardView Fomantic styling 0.0.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
+ */
+
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-se', 'datatables.net-cardview'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-se')(root);
+			}
+
+			if (! window.DataTable.CardView) {
+				require('datatables.net-cardview')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+Object.assign(DataTable.CardView.classes, {
+	card: 'ui fluid card dtcv-card',
+	cardContent: 'content'
+});
+
+
+return DataTable;
+}));
