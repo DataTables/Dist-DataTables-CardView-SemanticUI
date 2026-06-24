992a63da172dab18d42c1dfd3f936d9e455993d9
dtbuild <dtbuild@rigil>
2026-06-24 09:10:48 +0000

    48306b2287aa5f9788675d6d3a0f07f6e1e1f4c3 New: Destroy support
    I think this wraps up CardView for v1!
    
    edbe750d6888353afb2843994ccd2ae75c0d3a9e Almost ready for v1
    
    Sync to source repo @edbe750d6888353afb2843994ccd2ae75c0d3a9e

--- js/cardView.semanticui.min.mjs
+++ js/cardView.semanticui.min.mjs
@@ -1,4 +1,4 @@
-/*! CardView Fomantic styling 0.0.1 for DataTables
+/*! CardView Fomantic styling 1.0.0-dev for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 import DataTable from"datatables.net-se";import CardView from"datatables.net-cardview";Object.assign(DataTable.CardView.classes,{card:"ui fluid card dtcv-card",cardContent:"content"});export default DataTable;
